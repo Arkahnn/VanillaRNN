4e8298010b80f7ab9bd953227953be5d38ee492e
aretor <torci@live.it>
2017-07-20 22:13:52 +0200

    fixed imports

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import matplotlib.pyplot as plt
-from VanillaRNN import *
-from Tools import *
+import numpy as np
+import random
+
+import VanillaRNN
+import tools
 
 
-# This is the correct way to create a main function in Python (simply run it)
 if __name__ == "__main__":
+    # Set a seed to test the network. After having tested it, you can take it out
+    np.random.seed(314)
+    random.seed(314)
     K, eta, H_size, mini_batch_size = 100, 0.15, 100, 500
 
     print('Dataset creation')
     # Create dictionary
-    dictionary, train, valid, test = build_dictionary('Dataset.txt')
+    dictionary, train, valid, test = tools.build_dictionary('Dataset.txt')
 
     print('RNN initialization')
     # Initialize RNN
-    myRnn = MyRNN(dictionary, train, valid, test, H_size, eta)
-
+    myRnn = VanillaRNN.RNN(dictionary, train, valid, test, H_size, eta)
 
     print('Train the RNN')
     # Train the RNN
-    lossTrain, lossVal = myRnn.training_step(K, mini_batch_size)
+    loss_train, loss_val = myRnn.training(K, mini_batch_size)
 
     print('Test the RNN')
     # Test the RNN
-    lossTest = myRnn.test_step()
+    loss_test = myRnn.testing()
 
     # Save last weights in a file
     np.savetxt('Vmat.txt', myRnn.V, delimiter=',')
@@ -30,41 +34,28 @@ if __name__ == "__main__":
     np.savetxt('Wmat.txt', myRnn.W, delimiter=',')
 
     loss = ''
-    for s in lossTrain:
+    for s in loss_train:
         loss += str(s) + ', '
     file = open('lossTrain.txt', 'w')
     file.write(loss)
     file.close()
 
     loss = ''
-    for s in lossVal:
+    for s in loss_val:
         loss += str(s) + ', '
-    file = open('lossVal.txt', 'w')
-    file.write(loss)
-    file.close()
-    print('Loss value of the test set: ', lossTest)
+
+    with open('lossVal.txt', 'w') as f:
+        f.write(loss)
+
+    print('Loss value of the test set: ', loss_test)
 
     t = list(range(K))
-    plt.plot(t, lossTrain, 'ro', label='Training Set')
-    plt.plot(t, lossVal, 'g^', label='Validation Set')
+    plt.plot(t, loss_train, 'ro', label='Training Set')
+    plt.plot(t, loss_val, 'g^', label='Validation Set')
     plt.xlabel('time')
     plt.ylabel('loss value')
-    plt.title('Loss function computation')
+    plt.title('Loss function trend')
     plt.grid(True)
     plt.legend()
     # plt.savefig("test.png")
     plt.show()
-
-    # Multi-line comments are triggered with CTRL + /
-    # for i in range(K):
-    #     print('Epoch ', i, ':')
-    #     myRnn.fwdRnn()
-    #     Vnew, Unew, Wnew = myRnn.bwRnn()
-    #     # V = myRnn.V
-    #     myRnn.V, myRnn.U, myRnn.W = Vnew, Unew, Wnew
-    #     # print('Vecchio valore di V: ', V)
-    #     # print('Nuovo valore di V: ', (myRnn.V == Vnew))
-    #     # print('Valore di O: ', myRnn.O.sum())
-    #     loss_val += [myRnn.lossFunction(myRnn.Y, myRnn.O, myRnn.N)]
-    #     myRnn.S = np.zeros((myRnn.N, myRnn.T, myRnn.H))
-    #     myRnn.O = np.zeros((myRnn.N, myRnn.T, myRnn.D))
